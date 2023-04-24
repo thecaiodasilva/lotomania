4b3c62c678db846f947b1d1589d74e4a5d2bb7a7
Caio da Silva <66576487+thecaiodasilva@users.noreply.github.com>
2023-04-24 16:23:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+let numbers = [];
+
+function getRandomNumber() {
+
+    let randomNumber = Math.floor(Math.random() * 60) + 1;
+
+    if (numbers.includes(randomNumber)) {
+
+        return getRandomNumber();
+
+    }
+
+    numbers.push(randomNumber);
+
+    return randomNumber;
+
+}
+
+document.getElementById('rollButton').onclick = function() {
+
+    numbers = [];
+
+    let sorteios = [];
+
+    for (let i = 0; i < 15; i++) {
+
+        sorteios.push(getRandomNumber());
+
+    }
+
+    sorteios.sort(function(a, b) {
+
+        return a - b;
+
+    });
+
+    for (let i = 0; i < 15; i++) {
+
+        document.getElementById("sorte" + (i+1)).innerHTML = sorteios[i];
+
+    }
+
+};
+
